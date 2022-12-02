aa6da558c70e11b1f325015e1d56aeaa1fea947d
Gerardo Cipriano - gerardo.cipriano@studio.unibo.it <gerardo.cipriano@studio.unibo.it>
2022-12-02 14:22:18 +0100

    deleted vite structure

--- frontend/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <nav>
-    <RouterLink to="/">Homepage</RouterLink>
-    <RouterLink to="/menu">Menu</RouterLink>
-    <RouterLink to="/">Storico</RouterLink>
-    <RouterLink to="/">Prenota</RouterLink>
-  </nav>
-  <RouterView />
-</template>
