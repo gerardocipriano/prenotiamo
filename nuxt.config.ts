d657e977da4ee74213e168faa666b75cd607be54
unknown <gerardo.cipriano@studio.unibo.it>
2022-12-02 23:40:51 +0100

    added favicon.ico

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
+import { Head } from './.nuxt/components.d';
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-    
+export default ({
+
 })
 
 // generating the html page fot 404 code
