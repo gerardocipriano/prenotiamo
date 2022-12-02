5306bf4d755df2847dc24baa9381308931cd05fa
unknown <gerardo.cipriano@studio.unibo.it>
2022-12-02 21:44:29 +0100

    added error layout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    
 })
+
+// generating the html page fot 404 code
+generate: { fallback: '404.html' }  
