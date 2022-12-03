b0f2b70817e6ac8961b44bcdb5ed696b424fd4a9
FIRST_NAME LAST_NAME <massimilian.battelli@studio.unibo.it>
2022-12-03 23:14:59 +0100

    changed defaul.vue and index.vue graphics, also added tailwind.config.js file for importing external forms plugin

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,27 +4,23 @@
             <Meta name="description" content="Prenotiamo!.app" />
     </Head>
         <header>
-            <div class="pt-5 pb-5 bg-sky-400 flex justify-center">
-                <img class="" src="/img/splash_screen.jpeg" width="120" height="150" alt="prenotiamo splash screen" />
-                <h1 class="text-7xl">Prenotiamo!</h1>
+            <div class="pt-5 pb-5 bg-sky-400 grid grid-cols-13">
+                <img class="col-start-5" src="/img/splash_screen.jpeg" width="120" height="150" alt="prenotiamo splash screen" />
+                <div class="text-7xl col-end-7 font-semibold">Prenotiamo!</div>
+                <div class="text-3xl col-start-11 pt-5 font-semibold">Login</div>
             </div>
         </header>
-        <div class="flex flex-row">
-            <nav  class="basis-1/8">
-                <ul>
-                    <li><NuxtLink to="/">Home</NuxtLink></li>
-                    <li><NuxtLink to="/menu">Menu</NuxtLink></li>
-                    <li><NuxtLink to="/prenota">Prenota</NuxtLink></li>
-                    <li><NuxtLink to="/storico">Storico</NuxtLink></li>
+            <nav>
+                <ul class="flex flex-row justify-center">
+                    <li class="basis-20 font-medium"><NuxtLink to="/">Home</NuxtLink></li>
+                    <li class="basis-20 font-medium"><NuxtLink to="/menu">Menu</NuxtLink></li>
+                    <li class="basis-20 font-medium"><NuxtLink to="/prenota">Prenota</NuxtLink></li>
+                    <li class="basis-20 font-medium"><NuxtLink to="/storico">Storico</NuxtLink></li>
                 </ul>
             </nav>
-            <div class=" basis-7/8 grow text-3xl text-center bg-gray-300">
+            <div class="text-3xl text-center bg-gray-100">
                 <slot />
             </div>
-        </div>
+       
 </template>
 
-
-<style lang="scss" scoped>
-
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,13 @@
 <template>
-    <h1>Homepage</h1>
+    <h1 class="text-3xl font-medium mb-10">LOGIN</h1>
+    <div class="flex flex-ro justify-center">
+        <div class="mx-5">Username</div>
+        <input type="text" class="form-input px-4 py-3 rounded-full">
+    </div>
+    <div class="flex flex-ro justify-center my-5">
+        <div class="mx-5">Password</div>
+        <input type="password" class="form-input px-4 py-3 rounded-full">
+    </div>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Login</button>
 </template>
 
