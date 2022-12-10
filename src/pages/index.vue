b9d7e94de62423b0c05aed5a2f0962b2d5426bf1
unknown <gerardo.cipriano@studio.unibo.it>
2022-12-10 17:25:14 +0100

    modified img folder

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,7 +6,7 @@
         <div class="card" style="border-radius: 1rem;">
           <div class="row g-0">
             <div class="col-md-6 col-lg-5 d-none d-md-block">
-              <img src="/img/login_splashpage.png"
+              <img src="/img/login_splashpage.jpg"
                 alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
             </div>
             <div class="col-md-6 col-lg-7 d-flex align-items-center">
