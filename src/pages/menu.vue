0eccaddb5fbf9986cf2f67048b91adc9016d7a0e
FIRST_NAME LAST_NAME <massimilian.battelli@studio.unibo.it>
2022-12-12 12:22:19 +0100

    refactor menu.vue code

--- src/pages/menu.vue
+++ src/pages/menu.vue
@@ -4,32 +4,29 @@ import { Piatto } from "../types"
 export default defineComponent({
   data() {
     return {
-      Menu: [] as Piatto[],
+      Portate: {
       Antipasti: [] as Piatto[],
       Primi: [] as Piatto[],
       Secondi: [] as Piatto[],
       Pizze: [] as Piatto[]
+      }
     }
   },
   methods: {
-    getMenu() {
-      $fetch("/api/menu/").then(response => this.Menu = response as Piatto[])
-    },
     getAntipasti() {
-      $fetch("/api/menu/antipasti").then(response => this.Antipasti = response as Piatto[])
+      $fetch("/api/menu/antipasti").then(response => this.Portate.Antipasti = response as Piatto[])
     },
     getPrimi() {
-      $fetch("/api/menu/primi").then(response => this.Primi = response as Piatto[])
+      $fetch("/api/menu/primi").then(response => this.Portate.Primi = response as Piatto[])
     },
     getSecondi() {
-      $fetch("/api/menu/secondi").then(response => this.Secondi = response as Piatto[])
+      $fetch("/api/menu/secondi").then(response => this.Portate.Secondi = response as Piatto[])
     },
     getPizze() {
-      $fetch("/api/menu/pizze").then(response => this.Pizze = response as Piatto[])
+      $fetch("/api/menu/pizze").then(response => this.Portate.Pizze = response as Piatto[])
     },
   },
   mounted() {
-    this.getMenu()
     this.getAntipasti()
     this.getPrimi()
     this.getSecondi()
@@ -39,87 +36,23 @@ export default defineComponent({
 </script>
 <template>
 <div class="container text-center">
-  <h1>Antipasti</h1>
-    <div v-for="piatto in Antipasti" class="card mb-3">
-      <div class="row">
-        <div class="col-md-4">
-          <img src="/img/food_default.jpg" alt="" class="img-fluid rounded-start img-thumbnail" >
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h2 class="card-title">{{piatto.food_name}}</h2>
-            <p class="card-text">Price: {{piatto.price}}$</p>
-            <button type="button" class="btn btn-primary">Aggiungi</button>
+  <div v-for="portata,key in Portate">
+    <h1>{{key}}</h1>
+      <div v-for="piatto in portata" class="card mb-3">
+        <div class="row">
+          <div class="col-md-4">
+            <img src="/img/food_default.jpg" alt="" class="img-fluid rounded-start img-thumbnail" >
           </div>
-        </div>
-      </div> 
-    </div>
-    <h1>Primi</h1>
-    <div v-for="piatto in Primi" class="card mb-3">
-      <div class="row">
-        <div class="col-md-4">
-          <img src="/img/food_default.jpg" alt="" class="img-fluid rounded-start img-thumbnail" >
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h2 class="card-title">{{piatto.food_name}}</h2>
-            <p class="card-text">Price: {{piatto.price}}$</p>
-            <button type="button" class="btn btn-primary">Aggiungi</button>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h2 class="card-title">{{piatto.food_name}}</h2>
+              <p class="card-text">Price: {{piatto.price}}$</p>
+              <button type="button" class="btn btn-primary">Aggiungi</button>
+            </div>
           </div>
-        </div>
-      </div> 
-    </div>
-    <h1>Secondi</h1>
-    <div v-for="piatto in Secondi" class="card mb-3">
-      <div class="row">
-        <div class="col-md-4">
-          <img src="/img/food_default.jpg" alt="" class="img-fluid rounded-start img-thumbnail" >
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h2 class="card-title">{{piatto.food_name}}</h2>
-            <p class="card-text">Price: {{piatto.price}}$</p>
-            <button type="button" class="btn btn-primary">Aggiungi</button>
-          </div>
-        </div>
-      </div> 
-    </div>
-    <h1>Pizze</h1>
-    <div v-for="piatto in Pizze" class="card mb-3">
-      <div class="row">
-        <div class="col-md-4">
-          <img src="/img/food_default.jpg" alt="" class="img-fluid rounded-start img-thumbnail" >
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h2 class="card-title">{{piatto.food_name}}</h2>
-            <p class="card-text">Price: {{piatto.price}}$</p>
-            <button type="button" class="btn btn-primary">Aggiungi</button>
-          </div>
-        </div>
-      </div> 
-    </div>
+        </div> 
+      </div>
+  </div>
 </div>
 </template>
 
-<!-- <img :src="('/img/' + piatto.imgpiatto)" alt="" class="img-fluid rounded-start">
-<template>
-<div class="container text-center">
-  <h1>Antipasti</h1>
-    <div v-for="piatto in Menu" class="card mb-3">
-      <div class="row">
-        <div class="col-md-4">
-          <img src="/img/food_default.jpg" alt="" class="img-fluid rounded-start img-thumbnail" >
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h2 class="card-title">{{piatto.food_name}}</h2>
-            <p class="card-text">Price: {{piatto.price}}$</p>
-          </div>
-        </div>
-      </div> 
-    </div>
-</div>
-</template>
-
--->
