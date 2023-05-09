282379a9337af903a37441a81f9e3ed06c3ed5ae
gerardocipriano <gerardo.cipriano@studio.unibo.it>
2023-05-09 20:16:14 +0200

    test

--- src/types.ts
+++ src/types.ts
@@ -1,33 +1,32 @@
 export interface Piatto {
-    food_name: string
-    food_id: number
-    price: number
-    course: string
-    img_id: number
-  }
+  food_name: string
+  food_id: number
+  price: number
+  course: string
+  img_id: number
+}
 
-  export interface DailyOrder {
-    name: string
-    company: string
-    food_name: string
-    date: number
-    note: string
-    id: number
-  }
+export interface DailyOrder {
+  name: string
+  company: string
+  food_name: string
+  date: number
+  note: string
+  id: number
+}
 
-  export interface History {
-    food_name: string
-    price: number
-    note: string
-    day: number
-    month: number
-    year: number
-  }
-
-  export interface Company {
-    company_id: number
-    name: string
-  }
+export interface Company {
+  company_id: number
+  name: string
+}
+export interface History {
+  food_name: string
+  price: number
+  note: string
+  day: number
+  month: number
+  year: number
+}
 
 export interface User {
   id: number
